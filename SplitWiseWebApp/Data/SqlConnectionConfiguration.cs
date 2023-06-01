fcdf21811cbaec8b5dd2b9b8cfcccf2858c00505
satya584 <satyabratapujari1000@gmail.com>
2023-06-01 18:52:46 +0530

    Connect to MS sql server with project and done with connection string

---
+++ SplitWiseWebApp/Data/SqlConnectionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SplitWiseWebApp.Data
+{
+    public class SqlConnectionConfiguration
+    {
+        private readonly IConfiguration? _configuration;
+
+        private readonly string? _connectionString;
+
+        public SqlConnectionConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SqlDBcontext");
+            
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString); 
+    }
+}
--- SplitWiseWebApp/Program.cs
+++ SplitWiseWebApp/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using SplitWiseWebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<SqlConnectionConfiguration>();  
 
 var app = builder.Build();
 
