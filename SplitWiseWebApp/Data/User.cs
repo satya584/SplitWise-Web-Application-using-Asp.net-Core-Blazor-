05e729601a749027c53c791aec56d97cd6e5d6e9
satya584 <satyabratapujari1000@gmail.com>
2023-06-02 09:43:04 +0530

    adding of user class and service working good so far. no error

---
+++ SplitWiseWebApp/Data/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SplitWiseWebApp.Data
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+     
+        public DateTime createdAt { get; set; }= DateTime.Now;
+        public DateTime UpdatedAt { get; set; } =DateTime.Now;
+
+
+    }
+}
---
+++ SplitWiseWebApp/Data/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace SplitWiseWebApp.Data
+{
+    public class UserService
+    {
+        private readonly SqlConnectionConfiguration _connectionConfiguration;
+        public UserService(SqlConnectionConfiguration connectionConfiguration)
+        {
+
+            _connectionConfiguration = connectionConfiguration;
+
+        }
+
+        
+
+    }
+}
