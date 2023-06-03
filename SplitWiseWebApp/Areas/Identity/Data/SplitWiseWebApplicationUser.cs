162d16f89f16e8e9c16330e86e4ccd94f8c6f56e
satya584 <satyabratapujari1000@gmail.com>
2023-06-03 11:31:08 +0530

    Done with login And Register and Working Good .

---
+++ SplitWiseWebApp/Areas/Identity/Data/SplitWiseWebApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SplitWiseWebApp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the SplitWiseWebApplicationUser class
+public class SplitWiseWebApplicationUser : IdentityUser
+{
+
+}
+
--- SplitWiseWebApp/Program.cs
+++ SplitWiseWebApp/Program.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SplitWiseWebApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SplitWiseWebApp.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SplitWiseWebAppContextConnection") ?? throw new InvalidOperationException("Connection string 'SplitWiseWebAppContextConnection' not found.");
+
+builder.Services.AddDbContext<SplitWiseWebAppContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<SplitWiseWebApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<SplitWiseWebAppContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -27,5 +35,6 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapRazorPages();
 
 app.Run();
